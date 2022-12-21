22043ceb3f9bca37b910fdfd9bf32da2a0ced94c
Georgii Vlasov <honest.niceman@gmail.com>
2022-12-21 11:22:15 +0400

    Lab 7 minor changes

--- src/main/java/honest/niceman/university/lab7/Main.java
+++ src/main/java/honest/niceman/university/lab7/Main.java
@@ -3,36 +3,34 @@ package honest.niceman.university.lab7;
 import java.util.ArrayList;
 import java.util.List;
 
+//4. Напишите метод возвращающий все подмножества заданного множества
 public class Main {
-    private List<List<Integer>> output = new ArrayList<>();
-    private int n, k;
-
     public static void main(String[] args) {
         Main main = new Main();
-        System.out.println(main.subsets(new int[] {1, 2, 3}));
-    }
-
-    public void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
-        // if the combination is done
-        if (curr.size() == k) {
-            output.add(new ArrayList<>(curr));
-            return;
-        }
-        for (int i = first; i < n; ++i) {
-            // add i into the current combination
-            curr.add(nums[i]);
-            // use next integers to complete the combination
-            backtrack(i + 1, curr, nums);
-            // backtrack
-            curr.remove(curr.size() - 1);
-        }
+        System.out.println(main.subsets(new int[] {1, 2, 3, 4}));
     }
 
     public List<List<Integer>> subsets(int[] nums) {
-        n = nums.length;
-        for (k = 0; k < n + 1; ++k) {
-            backtrack(0, new ArrayList<>(), nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int subSetSize = 0; subSetSize < nums.length + 1; subSetSize++) {
+            backtrack(result, 0, new ArrayList<>(), nums, subSetSize);
         }
-        return output;
+        return result;
+    }
+
+    public void backtrack(List<List<Integer>> result,
+                          int firstToCheck,
+                          ArrayList<Integer> currentList,
+                          int[] nums,
+                          int subSetSize) {
+        if (currentList.size() == subSetSize) {
+            result.add(new ArrayList<>(currentList));
+            return;
+        }
+        for (int i = firstToCheck; i < nums.length; ++i) {
+            currentList.add(nums[i]);
+            backtrack(result, i + 1, currentList, nums, subSetSize);
+            currentList.remove(currentList.size() - 1);
+        }
     }
 }
