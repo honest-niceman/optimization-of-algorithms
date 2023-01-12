d3d3bb40f5981e1b61aafeb26d40fcf84f32276d
Georgii Vlasov <honest.niceman@gmail.com>
2023-01-12 10:09:02 +0400

    Various testing assumptions

--- src/main/java/honest/niceman/university/exam/Main.java
+++ src/main/java/honest/niceman/university/exam/Main.java
@@ -6,26 +6,77 @@ import java.util.*;
 // Выяснить сколько строк являются окончанием любой другой строки.
 public class Main {
     public static void main(String[] args) {
-        String[] arr = new String[]{"abc", "ba", "ab", "bc", "cd", "c", "adas", "cdb"};
+        String[] arr = removeDuplicates(generateStrings(100, 3));
         System.out.println(withImprovements(arr));
         System.out.println(straightSolution(arr));
+        System.out.println(valera(arr));
+        System.out.println(genius(arr));
+    }
+
+    public static String[] generateStrings(int arraySize, int wordsMaxLength) {
+        int leftLimit = 97; // letter 'a'
+//        int rightLimit = 122; // letter 'z'
+        int rightLimit = 99; // letter 'z'
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder();
+        String[] res = new String[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            int targetStringLength = random.nextInt(wordsMaxLength) + 1;
+            for (int j = 0; j < targetStringLength; j++) {
+                int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            res[i] = buffer.toString();
+            buffer.setLength(0);
+        }
+        return res;
+    }
+
+    public static String[] removeDuplicates(String[] array) {
+        return new HashSet<>(Arrays.asList(array)).toArray(new String[0]);
+    }
+
+    public static int valera(String[] arr) {
+        System.out.println("Valera:"+Arrays.toString(arr));
+        Map<String, Integer> map = new HashMap<>();
+        for (String  s : arr) {
+            int length = s.toCharArray().length;
+            for (int i = 1; i <= length; i++) {
+                String key = s.substring(length - 1, length);
+                Integer value = map.get(key);
+                map.put(key, value == null ? 0 : ++value);
+            }
+        }
+        return map.values().stream().mapToInt(x -> x).sum();
     }
 
     public static int straightSolution(String[] arr) {
+        System.out.println("Straight:"+Arrays.toString(arr));
         Set<String> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 if (i == j) continue;
                 if (arr[i].endsWith(arr[j])) {
-                    set.add(arr[i]);
+//                    System.out.println(i + " --- " + j);
+                    set.add(arr[j]);
                     break;
                 }
             }
         }
+        String[] res = new String[set.size()];
+        int i = 0;
+        for (String s : set) {
+            res[i++] = s;
+        }
+        Arrays.sort(res);
+//        System.out.println(Arrays.toString(res));
         return set.size();
     }
 
+    //
     public static int withImprovements(String[] arr) {
+        System.out.println("Impr:"+Arrays.toString(arr));
         Map<Integer, List<String>> map = getMap(arr);
         List<Integer> keyList = new ArrayList<>(map.keySet());
         Set<String> result = new HashSet<>();
@@ -49,7 +100,7 @@ public class Main {
                         if (shortWords.get(k).charAt(0) < longWords.get(l).charAt(0)) {
                             break;
                         }
-                        System.out.println("Short:" + shortWords.get(k) + "; Long:" + longWords.get(l));
+//                        System.out.println("Short:" + shortWords.get(k) + "; Long:" + longWords.get(l));
                         //так как мы развернули слова, то теперь проверяем что они не заканчиваются, а начинаются на ...
                         if (longWords.get(l).startsWith(shortWords.get(k))) {
                             result.add(shortWords.get(k));
@@ -59,7 +110,16 @@ public class Main {
                 }
             }
         }
-        System.out.println(map);
+//        System.out.println(map);
+
+
+        String[] res = new String[result.size()];
+        int i = 0;
+        for (String s : result) {
+            res[i++] = s;
+        }
+        Arrays.sort(res);
+//        System.out.println(Arrays.toString(res));
         return result.size();
     }
 
@@ -92,4 +152,49 @@ public class Main {
             map.replace(key, strings);
         }
     }
+
+
+    private static class Node {
+        private Map<Character, Node> children = new HashMap<>();
+
+        public Map<Character, Node> getChildren() {
+            return children;
+        }
+    }
+
+    public static int genius(String[] arr) {
+        System.out.println("Genius:"+Arrays.toString(arr));
+        Node root = new Node();
+        for (String s : arr) {
+            StringBuilder sb = new StringBuilder(s);
+            sb.reverse();
+            Node node = root;
+            for (int j = 0; j < sb.length(); j++) {
+                node = addNode(node, sb.charAt(j));
+            }
+        }
+        int count = 0;
+        for (String s : arr) {
+            StringBuilder sb = new StringBuilder(s);
+            sb.reverse();
+            Node node = root;
+            for (int j = 0; j < sb.length(); j++) {
+                node = node.getChildren().get(sb.charAt(j));
+            }
+            if(!node.getChildren().isEmpty()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static Node addNode(Node current, char value) {
+        boolean flag = current.getChildren().containsKey(value);
+        if (!flag) {
+            Node node = new Node();
+            current.getChildren().put(value, node);
+            return node;
+        } else return current.getChildren().get(value);
+    }
+
 }
